0cd440b5b5e70df2e39a247e58508e01dc204f6c
ColinKorsmo <123098936+ColinKorsmo@users.noreply.github.com>
2023-01-23 12:03:29 -0600

    Add files via upload

--- Homework1.cs
+++ Homework1.cs
@@ -1,9 +1,21 @@
 ﻿namespace HelloWorld;
+using System;
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        Console.WriteLine("Homework1");
+        Console.WriteLine("Colin Korsmo");
+        Console.WriteLine("Homework 1");
+
+        //equation integers
+        //this is the value of x
+        double x = 2.5;
+        //this is the value of y
+        double y = 3.3;
+        //this is the equation to solve for z
+        double z = ((4*x*x)+(3*y));
+        //this writes the value of z
+        Console.WriteLine("The value of Z is: "+z);
     }
 }
